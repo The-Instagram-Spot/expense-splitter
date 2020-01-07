ed65137299e8a29bf45fc3389baf7852d458ab2f
Yuri Castro <castroyuri09@gmail.com>
2020-01-07 16:00:29 +0000

    fixed deleteing person

--- test/controllers/people_controller_test.rb
+++ test/controllers/people_controller_test.rb
@@ -1,7 +1,19 @@
-require 'test_helper'
-
-class PeopleControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+class PeopleController < ApplicationController
+    def create
+        @groups = Group.find(params[:group_id])
+        @person = @groups.people.create(person_params)
+        redirect_to group_path(@groups)
+    end
+    
+    def destroy
+        @groups = Group.find(params[:group_id])
+        @person = @groups.people.find(params[:id])
+        @person.destroy
+        redirect_to group_path(@groups)
+    end
+    
+    private
+        def person_params
+            params.require(:person).permit(:name, :email)
+        end
 end
