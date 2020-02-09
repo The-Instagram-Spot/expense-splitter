bd44777826dfd5b89b6c23cd0566b8db09a2d0af
elizabethchristman <ubuntu@ip-172-31-40-150.ec2.internal>
2020-02-09 20:17:43 +0000

    CRUD transactions

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,25 +1,13 @@
 class TransactionsController < ApplicationController
-    def show
-        @transactions = Transaction.find(params[:id])
-    end
-    
-    def new
-        @transactions = Transaction.new
-    end
-    
     def create
         @groups = Group.find(params[:group_id])
         @transactions = @groups.transactions.create(transaction_params)
         redirect_to group_path(@groups)
-        
-       # @transactions.save
-        #    redirect_to @transactions
-        
     end
     
     def destroy
         @groups = Group.find(params[:group_id])
-        @transactions = @groups.transcations.find(params[:id])
+        @transactions = @groups.transactions.find(params[:id])
         @transactions.destroy
         redirect_to group_path(@groups)
     end
